6c20a3f75242356d4a2d5b7a4e49bf0bf30208d2
femakc <97850166+femakc@users.noreply.github.com>
2023-08-25 00:52:13 +0300

    added Helm

--- app/main.py
+++ app/main.py
@@ -1,18 +1,37 @@
+import os
+import json
+import socket
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+RESULT_DICT = {
+    "HOSTNAME": socket.gethostname(),
+    "AUTHOR": "!!!ANONYMOUS!!!",
+    "UID": "!!!UID!!!"
+               }
+
+
+for key in os.environ:
+    RESULT_DICT[key] = os.environ[key]
+
+
+@app.get("/")
+def get_all():
+    return json.dumps(RESULT_DICT, indent=4)
+
+
 @app.get("/hostname")
 def get_hostname():
-    return {"hostname": "hostname"}
+    return RESULT_DICT["HOSTNAME"]
 
 
 @app.get("/author")
 def get_author():
-    return {"author": "author"}
+    return RESULT_DICT["AUTHOR"]
 
 
 @app.get("/id")
 def get_id():
-    return {"id": "id"}
+    return RESULT_DICT["UID"]
